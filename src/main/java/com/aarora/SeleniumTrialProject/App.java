cd7164467b17be9488b58a0d97c0773074bef216
Arpit Arora <arpit.arora89@live.com>
2020-07-27 21:49:53 +1000

    Added maven plugin to build and package all dependencies

--- src/main/java/com/aarora/SeleniumTrialProject/App.java
+++ src/main/java/com/aarora/SeleniumTrialProject/App.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import io.github.bonigarcia.wdm.ChromeDriverManager;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 /**
